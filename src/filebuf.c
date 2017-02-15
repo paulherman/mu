bd1ef26e106ccf54dba8f6bf4a77bed51816e101
Paul Herman <paulstefanherman@gmail.com>
2017-02-15 14:12:28 +0000

    Fix file buffer length detection

--- src/filebuf.c
+++ src/filebuf.c
@@ -11,13 +11,18 @@ bool file_buffer_read(struct file_buffer *fb, const char *path) {
   uint8_t buffer[4096];
   uint8_t *contents = NULL;
   size_t num_batches = 0;
+  size_t length = 0;
 
-  while (fread(buffer, 1, 4096, file) != 0) {
-    num_batches++;
-    contents = realloc(contents, (4096 * num_batches + 1) * sizeof(uint8_t));
-    memcpy(contents + 4096 * (num_batches - 1), buffer, sizeof(uint8_t) * 4096);
-    memset(buffer, 0, sizeof(buffer));
-  }
+  do {
+    size_t read_length = fread(buffer, sizeof(uint8_t), 4096, file);
+    if (read_length > 0) {
+      length += read_length;
+      num_batches++;
+      contents = realloc(contents, (4096 * num_batches) * sizeof(uint8_t));
+      memcpy(contents + 4096 * (num_batches - 1), buffer, sizeof(uint8_t) * read_length);
+      memset(buffer, 0, sizeof(buffer));
+    }
+  } while (!feof(file) && ferror(file) == 0);
 
   if (ferror(file) != 0) {
     free(contents);
@@ -26,7 +31,7 @@ bool file_buffer_read(struct file_buffer *fb, const char *path) {
   } else {
     fb->position = 0;
     fb->data = contents;
-    fb->length = num_batches * 4096;
+    fb->length = length;
     fclose(file);
     return true;
   }
@@ -37,7 +42,7 @@ void file_buffer_delete(struct file_buffer *fb) {
 }
 
 bool file_buffer_read_float(struct file_buffer *file, float *value) {
-  if (file->position + sizeof(float) >= file->length)
+  if (file->position + sizeof(float) > file->length)
     return false;
   *value = *(float *)&file->data[file->position];
   file->position += sizeof(float);
@@ -45,7 +50,7 @@ bool file_buffer_read_float(struct file_buffer *file, float *value) {
 }
 
 bool file_buffer_read_uint8(struct file_buffer *file, uint8_t *value) {
-  if (file->position + sizeof(uint8_t) >= file->length)
+  if (file->position + sizeof(uint8_t) > file->length)
     return false;
   memcpy(value, file->data + file->position, sizeof(uint8_t));
   file->position += sizeof(uint8_t);
@@ -53,7 +58,7 @@ bool file_buffer_read_uint8(struct file_buffer *file, uint8_t *value) {
 }
 
 bool file_buffer_read_uint16(struct file_buffer *file, uint16_t *value) {
-  if (file->position + sizeof(uint16_t) >= file->length)
+  if (file->position + sizeof(uint16_t) > file->length)
     return false;
   memcpy(value, file->data + file->position, sizeof(uint16_t));
   file->position += sizeof(uint16_t);
@@ -61,7 +66,7 @@ bool file_buffer_read_uint16(struct file_buffer *file, uint16_t *value) {
 }
 
 bool file_buffer_read_int32(struct file_buffer *file, int32_t *value) {
-  if (file->position + sizeof(int32_t) >= file->length)
+  if (file->position + sizeof(int32_t) > file->length)
     return false;
   memcpy(value, file->data + file->position, sizeof(int32_t));
   file->position += sizeof(int32_t);
@@ -69,7 +74,7 @@ bool file_buffer_read_int32(struct file_buffer *file, int32_t *value) {
 }
 
 bool file_buffer_read_int16(struct file_buffer *file, int16_t *value) {
-  if (file->position + sizeof(int16_t) >= file->length)
+  if (file->position + sizeof(int16_t) > file->length)
     return false;
   memcpy(value, file->data + file->position, sizeof(int16_t));
   file->position += sizeof(int16_t);
@@ -77,7 +82,7 @@ bool file_buffer_read_int16(struct file_buffer *file, int16_t *value) {
 }
 
 bool file_buffer_read_uint32(struct file_buffer *file, uint32_t *value) {
-  if (file->position + sizeof(uint32_t) >= file->length)
+  if (file->position + sizeof(uint32_t) > file->length)
     return false;
   *value = *(uint32_t *)&file->data[file->position];
   file->position += sizeof(uint32_t);
@@ -86,7 +91,7 @@ bool file_buffer_read_uint32(struct file_buffer *file, uint32_t *value) {
 
 bool file_buffer_read_array(struct file_buffer *file, void *arr, size_t element_size, size_t length) {
   size_t read_size = length * element_size;
-  if (file->position + read_size >= file->length)
+  if (file->position + read_size > file->length)
     return false;
   memcpy(arr, file->data + file->position, read_size);
   file->position += read_size;
@@ -98,7 +103,7 @@ bool file_buffer_read_string(struct file_buffer *file, char *str, size_t length)
 }
 
 bool file_buffer_read_bool(struct file_buffer *file, bool *value) {
-  if (file->position + sizeof(bool) >= file->length)
+  if (file->position + sizeof(bool) > file->length)
     return false;
   *value = *(bool *)&file->data[file->position];
   file->position += sizeof(bool);
